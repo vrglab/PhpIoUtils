5da69cc2e1423f0ac476d26c544208ee40508b70
Vrglab <vrglab1@gmail.com>
2026-04-28 22:40:46 +0200

    Initial commit

---
+++ src/io/Directory.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+namespace io;
+
+class Directory
+{
+  public static function exists(string $path): bool
+  {
+    $isDir = is_dir($path);
+
+    if (!$isDir)
+      return false;
+
+    return file_exists($path);
+  }
+}
---
+++ tests/PhpIoUtils/test.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+
+namespace PhpIoUtils;
+use io\Directory;
+
+class test
+{
+  public static function dirExists(string $path): bool
+  {
+    return Directory::exists($path);
+  }
+}
